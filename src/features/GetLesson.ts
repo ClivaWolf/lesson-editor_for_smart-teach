b19d1fe4f319e394e64209fee32b3a79c5cc6d9f
MotoIlyuha <motoilyuha@mail.ru>
2024-07-24 22:59:27 +0300

    For Cliva

--- src/features/GetLesson.ts
+++ src/features/GetLesson.ts
@@ -1,5 +1,6 @@
 import {Lesson} from "../shared/types/LessonType";
 
+// TODO: Эта функция асинхронно читает файлы из /Course_1/Lessons/lesson_[id].json
 export function GetLesson({lessonId}: { lessonId: string }): Promise<Lesson> {
     return new Promise((resolve, reject) => {
             setTimeout(() => {
---
+++ src/features/SetLesson.ts
@@ -0,0 +1,1 @@
+//TODO: Здесь будет логика записи урока с определенным id в папку /Course_1/Lessons/lesson_[id].json
